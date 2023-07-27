8121638c5fe2c8bcd6c0397d24cc8bc2739427a6
Corey Lowman <clowman1993@gmail.com>
2023-07-27 14:16:40 -0400

    Updating example

--- examples/mlp.rs
+++ examples/mlp.rs
@@ -30,20 +30,20 @@ fn main() {
     let dev: Cpu = Default::default();
 
     {
-        let const_structure = arch::ConstMlp::default();
-        let module = dev.build_module_ext::<f32>(const_structure);
+        let structure = arch::ConstMlp::default();
+        let module = dev.build_module_ext::<f32>(structure);
         let x: Tensor<Rank2<10, 3>, f32, _> = dev.sample_normal();
         let _: Tensor<Rank2<10, 10>, f32, _> = module.forward(x);
     }
 
     {
-        let const_structure = arch::DynMlp {
+        let structure = arch::DynMlp {
             l1: DynLinear::new(3, 5),
             act1: Default::default(),
             l2: DynLinear::new(5, 10),
             act2: Default::default(),
         };
-        let module = dev.build_module_ext::<f32>(const_structure);
+        let module = dev.build_module_ext::<f32>(structure);
         let x: Tensor<(Const<10>, usize), f32, _> = dev.sample_normal_like(&(Const, 3));
         let y: Tensor<(Const<10>, usize), f32, _> = module.forward(x);
         assert_eq!(y.shape(), &(Const, 10));
